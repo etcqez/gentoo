9a96a2fd3bc9b45d1c4f49f8caa6300441d674bc
etcqez <etcqez@outlook.com>
2024-07-21 12:27:49 +0800

    更新配置文件

--- install.sh
+++ install.sh
@@ -3,10 +3,11 @@ set -ue
 # portage/package
 [[ -a /etc/portage/package.accept_keywords ]] && sudo rm /etc/portage/package.accept_keywords -rf
 [[ -a /etc/portage/package.license ]] && sudo rm /etc/portage/package.license -rf
-[[ -a /etc/portage/package.use ]] && sudo rm /etc/portage/package.use -rf
+[[ ! -a /etc/portage/package.use ]] && sudo mkdir /etc/portage/package.use
+[[ -a /etc/portage/package.use ]] && sudo rm /etc/portage/package.use/* -rf
 sudo ln ~/mygentoo/package.accept_keywords /etc/portage/package.accept_keywords
 sudo ln ~/mygentoo/package.license /etc/portage/package.license
-sudo ln -s ~/mygentoo/package.use /etc/portage/package.use
+sudo ln ~/mygentoo/package.use/* /etc/portage/package.use/
 
 # binhost
 sudo ln -f ~/mygentoo/gentoobinhost.conf /etc/portage/binrepos.conf/gentoobinhost.conf
